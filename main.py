dc291f79f763fc90eabdb468b01420273c2ba7f4
Niharika-cod <kinnusharma.kiran@gmail.com>
2026-08-25 12:51:29 +0530

    set bugs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print("If your choiced value is same as the computer chosen then you will be a w
 
 # Selection if range -- >
 
-rng = int (input("Select range(assuming starting value is 1) = "))
+rng = int (input("Select range = "))
 
 num_list = []
 
@@ -23,37 +23,49 @@ comp_choice = comp_fn ()
 
 # user input -- >
 
+attempt = 1
+
+n = int (input ("No. of attempts (max-'10')= "))
+
+user_choice = int(input("Select any random number = "))
 
 
-n = int (input ("No. of attempt you challenge the computer to win = "))
-
-user_choice = int(input("Select any random number in your range = "))
-
-attempt = 0
 
 # Condn of winning -- >
 
 while True :
-    attempt += 1
-    if (attempt > n ) :
-        print (f"You did'nt complete your game in {n} attempts!")
-        break 
+    if (rng <= 0) :
+        print ("Invalid ")
+        break
+
+    elif ((n>10) or (n<=0)) :
+        print (f"Invalid attempt!")
+        break
+    
     elif (comp_choice == user_choice) :
-        print(f"Hurrah ! You win the game at {attempt} attempt as computer also choosed {comp_choice}  ")
+        print(f"Hurrah ! You win the game at {attempt}")
         break
-    elif (user_choice > rng):
-        print(f"Entered value is not in range of {rng}.Restart the game !!!")
-        break
-    elif (user_choice < 1):
-        print(f"Entered  is not in range of {rng}.Restart the game !!!")
+    elif ((user_choice) > rng or (user_choice < 1)):
+        print(f"Invalid number!")
         break
     elif (comp_choice > user_choice) :
-        print(f"Oops! your chosen value is smaller than computer choosen value.")
+        
+        attempt += 1
+        if (attempt > n ) :
+                    print (f"You did'nt complete your game in {n} attempts!")
+                    break 
+        print(f"Choose greater value")
         user_choice = int(input("Try again = "))
     elif (comp_choice < user_choice) :
-        print(f"Oops! your chosen value is greater than computer choosen value.")
+        
+        attempt += 1
+        if (attempt > n ) :
+                            print (f"You did'nt complete your game in {n} attempts!")
+                            break 
+        print(f"Choose smaller value")
         user_choice = int(input("Try again  = "))
-    
+
+print (f"computer choosed = {comp_choice}")
 print("---GAME OVER---")
     
         
